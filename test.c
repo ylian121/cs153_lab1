7c10e1750bbfa17f6e87141bee9c467de89db3ad
Yongyan Liang <ylian121@xe-09.cs.ucr.edu>
2024-07-21 02:48:35 -0700

    test

---
+++ test.c
@@ -0,0 +1,21 @@
+#include "types.h"
+#include "user.h"
+
+
+void getSibling(void){}
+
+int main(int argc, char *argv[])
+{
+    getSibling();
+    exit();
+}
+
+
+//lab 1 slide
+/*
+int main(int argc, char* argv[]){
+    //new added system call
+    hello();
+    exit();
+}
+*/
