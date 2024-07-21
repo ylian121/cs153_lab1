41111cbe08f955e1913099a12816813b8b78efb7
Yongyan Liang <ylian121@xe-09.cs.ucr.edu>
2024-07-21 02:40:52 -0700

    syscall

--- syscall.c
+++ syscall.c
@@ -103,8 +103,12 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
+
+extern int sys_getsiblings(void);
+
+
 //lab 1 slide
-extern int sys_hello(void);
+//extern int sys_hello(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
