e6ea515fdd96695e97e27798858cd5393bda41a2
Yongyan Liang <ylian121@xe-09.cs.ucr.edu>
2024-07-21 13:17:33 +0000

    test changed exit and wait

--- test_getsiblings.c
+++ test_getsiblings.c
@@ -7,28 +7,28 @@ int main() {
     if (child1_pid == 0) {      // make sure only the child process enters this block     
         sleep(100);             // delay to allow sibiling creation
         getsiblings();          // getsiblings() syscall
-        exit();                // Exit status added (lab1-part3)
+        exit(0);                // Exit status added (lab1-part3)
     }
 
     int child2_pid = fork();
     if (child2_pid == 0) {
         sleep(100);
-        exit();                // Exit status added (lab1-part3)
+        exit(0);                // Exit status added (lab1-part3)
     }
 
     int child3_pid = fork();
     if (child3_pid == 0) {
         sleep(100);
-        exit();                // Exit status added (lab1-part3)
+        exit(0);                // Exit status added (lab1-part3)
     }
 
     // Calling wait syscall ensures the parent process waits for 
     // all child processes to terminate, preventing zombie processes.
     int wait_status;
     int wait_pid; 
-    wait_pid = wait();   // Exit status added (lab1-part4)
-    wait_pid = wait();   // Exit status added (lab1-part4)
-    wait_pid = wait();   // Exit status added (lab1-part4)
+    wait_pid = wait(&wait_status);   // Exit status added (lab1-part4)
+    wait_pid = wait(&wait_status);   // Exit status added (lab1-part4)
+    wait_pid = wait(&wait_status);   // Exit status added (lab1-part4)
 
-    exit();                         // Exit status added (lab1-part3)
+    exit(0);                         // Exit status added (lab1-part3)
 }
